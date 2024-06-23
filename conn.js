9095975d2d2e58874fda23e8df6a9b8dfda958fd
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-06-24 02:03:45 +0530

    add api

--- conn.js
+++ conn.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/backendbookingsite');
+mongoose.connect('mongodb://rajendra0968jangid:71XRWGUdx1X7fRqf@ac-d1apbty-shard-00-00.hh3bywt.mongodb.net:27017,ac-d1apbty-shard-00-01.hh3bywt.mongodb.net:27017,ac-d1apbty-shard-00-02.hh3bywt.mongodb.net:27017/uniqueone?ssl=true&replicaSet=atlas-r6lhzh-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0');
 
 const con = mongoose.connection;
-
 con.on('open', () => {
     console.log("connection success");
 })
@@ -18,7 +17,18 @@ const formSchema = new mongoose.Schema({
 })
 
 const Form = mongoose.model('Form',formSchema)
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: {
+        type:String,
+        unique:true
+    },
+    password: String,
+    phone:String,
+})
 
-export {Form}
+const User = mongoose.model('User',userSchema)
+
+export {Form,User}
 
 
--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import cors from "cors"
 import bodyParser from "body-parser";
 import bcrypt from "bcrypt"
 import multer from "multer";
-import { Form } from "./conn.js"
+import { Form, User } from "./conn.js"
 import path from "path"
 import { fileURLToPath } from 'url';
 
+//use express 
 const app = express();
 
+//use for file send
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+//use for get input from api (req.body)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+//use for give access to use api any one
 app.use(cors());
 
+//use multer for file upload (get image in backend)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './upload')
@@ -26,31 +30,48 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + uniqueSuffix)
     }
 })
-
 const upload = multer({ storage: storage })
 
+//test api of backend
 app.get("/", async (req, res) => {
     try {
-        res.json({ data: "hello" })
+        res.json({ data: null, message: "Hello backend" })
     }
     catch (err) {
         console.log(err)
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
+//post api for user signup
 app.post("/signup", async (req, res) => {
     try {
         const { username, email, password, phone } = req.body;
-        const passwordB = await bcrypt.hash(password, 10);
-        console.log("password", password);
-        console.log("passwordB", passwordB);
-        return res.status(200).json({ data: "hello post" })
+        // const passwordB = await bcrypt.hash(password, 10);
+        const obj = {
+            username, email, password, phone
+        }
+        const newUser = new User(obj);
+        const userData = await newUser.save();
+        return res.status(200).json({ data: null, message: "User register successfully" })
     } catch (err) {
         console.log(err)
-        res.status(500).json({ data: err.message })
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
+app.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userData = await User.findOne({ email });
+        return res.status(200).json({ data: userData, message: "User log in successfully" })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ data: null, message: err.message })
+    }
+})
+
+//post api for booking form 
 app.post("/bookingform", upload.fields([{ name: 'hotelimage', maxCount: 1 }]), async (req, res) => {
     try {
         const { hotelemail, hotellocation, hotelname, hotelphone, hotelrent } = req.body;
@@ -67,46 +88,58 @@ app.post("/bookingform", upload.fields([{ name: 'hotelimage', maxCount: 1 }]), a
         console.log(formData);
         // console.log(req.body);
         // console.log(req.files);
-        res.status(200).json({ data: "hello form" })
+        res.status(200).json({ data: null, message: "Form submitted successfully" })
     }
     catch (err) {
         console.log(err)
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
+//get api for get booking data
 app.get('/bookingdata', async (req, res) => {
     try {
         const allData = await Form.find();
         res.status(200).json(allData)
     } catch (err) {
         console.log(err);
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
 
-// Route to serve the image
+// create image api for image get from backend
 app.get('/image/:imageName', (req, res) => {
-    const imageName = req.params.imageName;
-    const imagePath = path.join(__dirname, 'upload', imageName);
-    res.sendFile(imagePath);
+    try {
+
+        const imageName = req.params.imageName;
+        const imagePath = path.join(__dirname, 'upload', imageName);
+        res.sendFile(imagePath);
+    }
+    catch (err) {
+        res.status(500).json({ data: null, message: err.message })
+    }
 });
 
+//delete api for delete booking data by id
 app.delete("/bookingdatadelete/:id", async (req, res) => {
     try {
         const id = req.params.id
         const delData = await Form.deleteOne({ _id: id }); // returns {deletedCount: 1}
-        res.status(200).json({ data: "successfully deleted" })
+        res.status(200).json({ data: null, message: "Successfully deleted" })
     }
     catch (err) {
         console.log(err)
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
+//put api for update booking data by id
 app.put("/bookingdataedit/:id", upload.fields([{ name: 'hotelimage', maxCount: 1 }]), async (req, res) => {
     try {
         const id = req.params.id;
         console.log(req.body)
-        let checkImage = (req.files['hotelimage'])?req.files['hotelimage'][0].filename:false;
+        let checkImage = (req.files['hotelimage']) ? req.files['hotelimage'][0].filename : false;
         console.log(checkImage);
         const { hotelemail, hotellocation, hotelname, hotelphone, hotelrent } = req.body;
         const obj = {
@@ -115,25 +148,27 @@ app.put("/bookingdataedit/:id", upload.fields([{ name: 'hotelimage', maxCount: 1
             hotelemail: hotelemail,
             hotellocation: hotellocation,
             hotelrent: hotelrent,
-            hotelimage:(checkImage)?req.files['hotelimage'][0].filename:req.body.hotelimage
+            hotelimage: (checkImage) ? req.files['hotelimage'][0].filename : req.body.hotelimage
         }
 
         const editData = await Form.findByIdAndUpdate(id, obj, { new: true });
-        res.status(200).json({ data: "successfully updated" })
+        res.status(200).json({ data: null, message: "Successfully updated" })
     }
     catch (err) {
         console.log(err)
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 
+//get api for get booking data by id
 app.get('/bookingdatabyid/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const formData = await Form.findById(id).exec()
-        // console.log(formData);
         res.status(200).json(formData)
     } catch (err) {
         console.log(err)
+        res.status(500).json({ data: null, message: err.message })
     }
 })
 app.listen(8000)
